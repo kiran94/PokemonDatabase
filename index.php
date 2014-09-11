500ca807fca9ee4bd2bab3f1947203562a920c7f
Kiran Patel <kiranpatel@Kirans-MacBook-Pro.local>
2014-09-11 09:03:28 +0100

    added functionality to extract and parse json from the site

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Pokemon Database</title>
+	<link rel="stylesheet" type="text/css" href="/global.css" />
+	<link rel="stylesheet" type="text/css" href="style/style.css" />
+</head>
+<body>
+
+	<!-- HEADER -->
+	<div class='header'>
+		<h1 class='title'>Pokemon Database<span>By Kiran Patel</span></h1>
+	</div>
+	<!-- END HEADER -->
+
+	<!-- CONTENT -->
+	<div class='search'>
+		<form method='POST' action='#'>
+			<input type='text' name='pokemon' placeholder='Enter A Pokemon Name's />
+
+			<input type='submit' />
+		</form>
+	</div>
+	<!-- END CONTENT -->
+
+	<!-- FOOTER -->
+	<div class='footer'>
+
+	</div>
+	<!-- END FOOTER -->
+
+</body>
+</html>
